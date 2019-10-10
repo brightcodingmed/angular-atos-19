a0f80ed4701d41188a2d62f78a6e0ab1627d399f
brightcoding <elearndevelopement@gmail.com>
2019-10-10 03:00:07 +0100

    component and services

--- client-panel/src/app/app-routing.module.ts
+++ client-panel/src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ClientsListComponent } from './components/clients-list/clients-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ClientsAddComponent } from './components/clients-add/clients-add.component';
+import { ClientsEditComponent } from './components/clients-edit/clients-edit.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: '/clients', pathMatch: 'full' },
+  { path : "clients", component: ClientsListComponent },
+  { path : "clients/create", component: ClientsAddComponent },
+  { path : "clients/edit/:id", component: ClientsEditComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
